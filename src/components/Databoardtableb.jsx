e1c597f6e85a94de137f2549efdd684a322568ca
Atharv Gulati <atharvgulati@192.168.1.25>
2023-01-08 14:26:18 +0530

    new

---
+++ src/components/Databoardtableb.jsx
@@ -0,0 +1,160 @@
+import { useState } from "react";
+
+export function Databoardtableb() {
+
+  const [showInput, setShowInput] = useState(false);
+  const [buidlingSpace,setBuilding] = useState("");
+  const [distance, setDistance] = useState("");
+  const [dates, setDate] = useState('');
+
+  const handleChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const [lo, setLo] = useState([
+    {"date": "Jun 25, 2022","Total Building Space":1200,"distance":200},
+    {"date": "Jun 25, 2022","Total Building Space":1200,"distance":200},
+    {"date": "Jun 26, 2022","Total Building Space":1200,"distance":200},
+    {"date": "Jun 27, 2022","Total Building Space":1200,"distance":200},
+    {"date": "Jun 28, 2022","Total Building Space":1200,"distance":200},
+
+  ]);
+
+  return (
+    <>
+      <table className="databoardtable__table">
+        <thead className="databoardtable__tablehead">
+          <tr className="databoardtable__theadtr">
+            {/* <th className="databoardtable__theadth" scope="col">
+                <span className="blank__letters">check</span>
+              </th> */}
+            <th className="databoardtable__theadth" scope="col">
+              Date
+            </th>
+            <th className="databoardtable__theadth" scope="col">
+              Total Building Space
+            </th>
+            <th className="databoardtable__theadth" scope="col">
+              Type Warehouse Space
+            </th>
+          </tr>
+        </thead>
+        <tbody className="databoardtable__tablebody"> 
+          {[...lo].map((item, idx) => {
+            return (
+              <tr key={idx} className="databoardtable__tabletr">
+                <td className="databoardtable__tabletd">{item.date}</td>
+                <td className="databoardtable__tabletd">{item["Total Building Space"]}</td>
+                <td className="databoardtable__tabletd">{item.distance}&nbsp;kms</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        </table>
+        <div style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          padding:"8px",
+        }}>{
+          showInput ? (
+            <div style={{fontSize: "14px"}}>
+              Enter Date
+              <input
+                type="date"
+                value={dates}
+                onChange={handleChange}
+                style={{
+                  appearance: 'none',
+                  border: 'solid 0.5px',
+                  borderRadius: '4px',
+                  boxShadow: 'inset 0 0 0 1px rgba(0,0,0,0.1)',
+                  padding: '8px',
+                  margin: '0px 8px 16px 8px',
+                }}
+              />
+              Enter Building Space
+              <input type={"number"} value={buidlingSpace} style={{
+                appearance: 'none',
+                border: 'solid 0.5px',
+                padding:"8px",
+                margin: "0px 8px 16px 8px",
+                borderRadius: "4px",
+                height: "30px"
+              }} required={true} onChange = {(e) => {
+                const value = parseInt(e.target.value)  ;
+                if(value > -1){
+                  setBuilding(value)
+                }
+                else if(e.target.value === ""){
+                  setBuilding("")
+                }
+              }}/> 
+              Enter Warehouse Space
+              <input type={"number"} value={distance} style={{
+                appearance: 'none',
+                border: 'solid 0.5px',
+                padding:"8px",
+                margin: "0px 8px 16px 8px",
+                borderRadius: "4px",
+                height: "30px"
+              }} required={true} onChange = {(e) => {
+                const value = parseInt(e.target.value)  ;
+                if(value > -1){
+                  setDistance(value)
+                }
+                else if(e.target.value === ""){
+                  setDistance("")
+                }
+              }}/> 
+            
+            <button id="addButton" style={{
+              padding:"8px",
+              borderRadius: "4px",
+              color: "white",
+              backgroundColor: "#4d7cfe",
+              }}
+              onClick={() => {
+                /*const change = document.querySelector(".databoardtable__tablebody")
+                change.innerHTML = change.innerHTML + `
+                <tr class="databoardtable__tabletr">
+                <td class="databoardtable__tabletd">25 Jun 2022</td>
+                <td class="databoardtable__tabletd">
+                ${energy} kWh
+                </td>
+                <td class="databoardtable__tabletd">Coal</td>
+                <td class="databoardtable__tabletd">-</td>
+                <td class="databoardtable__tabletd">coming soon</td>
+              </tr>
+                `*/
+                if(distance > -1 && distance !== "" && dates !== "" && buidlingSpace > -1 && buidlingSpace !== ""){
+                  const dateString = dates;
+                  const date = new Date(dateString);
+                  const options = {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  };
+                  
+                  const formattedDate = date.toLocaleDateString("en-US", options);
+                  setLo([...lo,{"date": formattedDate,"Total Building Space":buidlingSpace,"distance":distance}]);
+                  setShowInput(false)
+                }
+              }}
+              >Add Row</button></div>
+          ) : (
+            <button id="addButton" style={{
+            padding:"8px",
+            borderRadius: "4px",
+            color: "white",
+            backgroundColor: "#4d7cfe",
+            display: "hidden"
+            }}
+            onClick={() => setShowInput(true)}
+            >Add Row</button>)}
+        </div>
+        
+    </>
+  );
+}
---
+++ src/components/Databoardtablep.jsx
@@ -0,0 +1,157 @@
+import { useState } from "react";
+
+export function Databoardtablep() {
+
+  const [showInput, setShowInput] = useState(false);
+  const [fuelType, setFuel] = useState("");
+  const [distance, setDistance] = useState("");
+  const [dates, setDate] = useState('');
+
+  const handleChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  const [lo, setLo] = useState([
+    {"date": "Jun 25, 2022","fuelType":"Maternity Bra","distance":200},
+    {"date": "Jun 25, 2022","fuelType":"Maternity Bra","distance":200},
+    {"date": "Jun 26, 2022","fuelType":"Maternity Bra","distance":200},
+    {"date": "Jun 27, 2022","fuelType":"Maternity Bra","distance":200},
+    {"date": "Jun 28, 2022","fuelType":"Maternity Bra","distance":200},
+
+  ]);
+
+  return (
+    <>
+      <table className="databoardtable__table">
+        <thead className="databoardtable__tablehead">
+          <tr className="databoardtable__theadtr">
+            {/* <th className="databoardtable__theadth" scope="col">
+                <span className="blank__letters">check</span>
+              </th> */}
+            <th className="databoardtable__theadth" scope="col">
+              Date
+            </th>
+            <th className="databoardtable__theadth" scope="col">
+              Type of Product
+            </th>
+            <th className="databoardtable__theadth" scope="col">
+              Quantity
+            </th>
+          </tr>
+        </thead>
+        <tbody className="databoardtable__tablebody"> 
+          {[...lo].map((item, idx) => {
+            return (
+              <tr key={idx} className="databoardtable__tabletr">
+                <td className="databoardtable__tabletd">{item.date}</td>
+                <td className="databoardtable__tabletd">{item.fuelType}</td>
+                <td className="databoardtable__tabletd">{item.distance}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        </table>
+        <div style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          padding:"8px",
+        }}>{
+          showInput ? (
+            <div style={{fontSize: "14px"}}>
+              Enter Date
+              <input
+                type="date"
+                value={dates}
+                onChange={handleChange}
+                style={{
+                  appearance: 'none',
+                  border: 'solid 0.5px',
+                  borderRadius: '4px',
+                  boxShadow: 'inset 0 0 0 1px rgba(0,0,0,0.1)',
+                  padding: '8px',
+                  margin: '0px 8px 16px 8px',
+                }}
+              />
+              Enter Type Of Product
+              <input type={"text"} value={fuelType} style={{
+                appearance: 'none',
+                border: 'solid 0.5px',
+                padding:"8px",
+                margin: "0px 8px 16px 8px",
+                borderRadius: "4px",
+                height: "30px"
+              }} required={true} onChange = {(e) => {
+                const value = (e.target.value)  ;
+                setFuel(value) ;
+              }}/> 
+              <br/>
+              Enter Quantity
+              <input type={"number"} value={distance} style={{
+                appearance: 'none',
+                border: 'solid 0.5px',
+                padding:"8px",
+                margin: "0px 8px 16px 8px",
+                borderRadius: "4px",
+                height: "30px"
+              }} required={true} onChange = {(e) => {
+                const value = parseInt(e.target.value)  ;
+                if(value > -1){
+                  setDistance(value)
+                }
+                else if(e.target.value === ""){
+                  setDistance("")
+                }
+                
+              }}/> 
+            
+            <button id="addButton" style={{
+              padding:"8px",
+              borderRadius: "4px",
+              color: "white",
+              backgroundColor: "#4d7cfe",
+              }}
+              onClick={() => {
+                /*const change = document.querySelector(".databoardtable__tablebody")
+                change.innerHTML = change.innerHTML + `
+                <tr class="databoardtable__tabletr">
+                <td class="databoardtable__tabletd">25 Jun 2022</td>
+                <td class="databoardtable__tabletd">
+                ${energy} kWh
+                </td>
+                <td class="databoardtable__tabletd">Coal</td>
+                <td class="databoardtable__tabletd">-</td>
+                <td class="databoardtable__tabletd">coming soon</td>
+              </tr>
+                `*/
+                if(distance > -1 && distance !== "" && dates !== "" && fuelType!== ""){
+                  const dateString = dates;
+                  const date = new Date(dateString);
+                  const options = {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  };
+                  
+                  const formattedDate = date.toLocaleDateString("en-US", options);
+                  setLo([...lo,{"date": formattedDate,"fuelType":fuelType,"distance":distance}]);
+                  setShowInput(false)
+                }
+              }}
+              >Add Row</button></div>
+          ) : (
+            <button id="addButton" style={{
+            padding:"8px",
+            borderRadius: "4px",
+            color: "white",
+            backgroundColor: "#4d7cfe",
+            display: "hidden"
+            }}
+            onClick={() => setShowInput(true)}
+            >Add Row</button>)}
+        </div>
+        
+    </>
+  );
+}
